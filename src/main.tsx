6cfe5a64895b1f61f025e750754ca79c68bce8bb
ronakjesadiya <RONAKJESADIYA.9@GMAIL.COM>
2024-04-02 22:10:25 +0530

    Created a test button for shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Relax from './pages/relax'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Relax/>
   </React.StrictMode>,
 )
---
+++ src/pages/relax.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+export const Relax = () => {
+  return (
+    <div>
+        <Button className='px-3 m-4'>Click</Button>
+    </div>
+  )
+}
+
+export default Relax;
